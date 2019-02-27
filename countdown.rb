b2338ea7151a566cd0c1fd3b132e3fd8b484b63d
delightful-tree-6762 <mduckworth@gmail.com>
2019-02-27 06:44:18 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,17 @@
 #write your code here
 
-def countdown
-  def countdown(seconds)
-  while seconds > 0
-    puts "#{seconds} SECOND(S)!"
-    seconds -= 1
+def countdown(count)
+    while count >0
+      puts "#{count} seconds!"
+      count-=1
+    end
+    "0 left. HAPPY NEW YEAR!"
   end
-  return "HAPPY NEW YEAR!"
-end
+
+  def countdown_with_sleep(count)
+    while count > 0
+      count-=1
+      sleep(1)
+    end
+      puts "0 left. HAPPY NEW YEAR!"
+  end
